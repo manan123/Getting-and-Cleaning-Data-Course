901b5b8ff0184d237d7ded8a39d800315e6eab86
Umesh <monukaushik@gmail.com>
2017-06-28 21:05:33 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -61,6 +61,6 @@ all_data <- cbind(x_data, y_data, subject_data)
 # for each activity and each subject
 
 # 66 <- 68 columns but last two (activity & subject)
-averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+avg_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
-write.table(averages_data, "averages_data.txt", row.name=FALSE)
+write.table(averages_data, "avg_data.txt", row.name=FALSE)
